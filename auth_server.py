90796a82607ce105c2997d858a85729a535df6af
lilyman266 <felixgeorgew2@gmail.com>
2026-02-06 15:27:50 -0500

    Attempt to make login via users DB work

--- auth_server.py
+++ auth_server.py
@@ -11,7 +11,8 @@ from DatabaseModule.Database.database_stub import WebAppDatabaseStub
 
 # Create the Flask application instance
 app = Flask(__name__)
-stub = AuthStubUserTable()
+# stub = AuthStubUserTable()    #NOTE: COMMENT ONE OF THESE AT A TIME
+stub = AuthDbStubTest()
 
 # ###################### AUTH SERVER REDIRECTS ########################################
 @app.route('/', methods=['GET'])
