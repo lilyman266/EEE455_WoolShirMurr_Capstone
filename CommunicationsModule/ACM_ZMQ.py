48e9c49ff6a558c78d534a09c235a5ac6d1820be
s30711 <samspambythefoot@gmail.com>
2026-03-11 09:38:47 -0400

    fixed GSCM_ZMQ and ACM_ZMQ

--- CommunicationsModule/ACM_ZMQ.py
+++ CommunicationsModule/ACM_ZMQ.py
@@ -7,29 +7,8 @@ from CommunicationsModule.CommunicationsProtocol.DataLinkLayer.DataLinkLayer imp
 from CommunicationsModule.CommunicationsProtocol.SessionLayer.SessionLayer import AudimusSessionLayer
 
 
-def read_lines(path):
-    with open(path, "r", encoding="utf-8") as f:
-        for line in f:
-            yield line.rstrip("\n")
-
-async def app_rx(AL_rx):
-    while True:
-        message = await AL_rx.get()
-
-async def app_tx(AL_tx):
-    for line in read_lines("CommunicationsModule/TestTXAudimus"):
-        await AL_tx.put(line)
-        await asyncio.sleep(1)
-
-
 
 async def run_client(reader =1, writer =2):
-
-
-    #Application layer queues
-    AL_rx = asyncio.Queue()
-    AL_tx = asyncio.Queue()
-
     # presentation layer queues
     PL_rx = asyncio.Queue()
     PL_tx = asyncio.Queue()
@@ -38,7 +17,7 @@ async def run_client(reader =1, writer =2):
     SL_rx = asyncio.Queue()
     SL_tx = asyncio.Queue()
 
-    #session state change queue
+    # session state change queue
     SL_sc = asyncio.Queue()
 
     # data link layer queues
@@ -50,35 +29,27 @@ async def run_client(reader =1, writer =2):
     SDR_tx = asyncio.Queue()
 
     # create instances of each layer
-    al = AudimusApplicationLayer(AL_rx, AL_tx,PL_rx,PL_tx)
+    al = AudimusApplicationLayer(PL_rx, PL_tx)
     pl = AudimusPresentationLayer(PL_rx, PL_tx, SL_rx, SL_tx)
     sl = AudimusSessionLayer(SL_rx, SL_tx, DLL_rx, DLL_tx, SL_sc)
     dll = AudimusDataLinkLayer(DLL_rx, DLL_tx, SDR_rx, SDR_tx, reader, writer)
 
-    # run application rx and tx coroutines
-    AL_rx_handler = asyncio.create_task(app_rx(AL_rx))
-    AL_tx_handler = asyncio.create_task(app_tx(AL_tx))
-
     # run application layer coroutines
-    al_tx_handler = asyncio.create_task(al.tx())
+    al_tx_handler = asyncio.create_task(al.tx_file())
     al_rx_handler = asyncio.create_task(al.rx())
-
 
     # run presentation layer coroutines
     pl_tx_handler = asyncio.create_task(pl.tx())
     pl_rx_handler = asyncio.create_task(pl.rx())
 
     # session layer coroutines started internally
-    # session layer coroutines started internally
     sl_handler = asyncio.create_task(sl.start())
 
     # run data link layer coroutines
-    dll_tx_handler = asyncio.create_task(dll.tx_tcp())
-    dll_rx_handler = asyncio.create_task(dll.rx_tcp())
+    dll_tx_handler = asyncio.create_task(dll.tx_zmq())
+    dll_rx_handler = asyncio.create_task(dll.rx_zmq())
 
     task_handlers = [
-        AL_rx_handler,
-        AL_tx_handler,
         al_tx_handler,
         al_rx_handler,
         pl_tx_handler,
--- CommunicationsModule/GSCM_ZMQ.py
+++ CommunicationsModule/GSCM_ZMQ.py
@@ -7,93 +7,47 @@ from CommunicationsModule.CommunicationsProtocol.DataLinkLayer.DataLinkLayer imp
 from CommunicationsModule.CommunicationsProtocol.SessionLayer.SessionLayer import GroundStationSessionLayer
 
 
-def read_lines(path):
-    with open(path, "r", encoding="utf-8") as f:
-        for line in f:
-            yield line.rstrip("\n")
-
-
-async def app_rx(AL_rx):
-    while True:
-        message = await AL_rx.get()
-
-
-async def app_tx(AL_tx, sl ):
-
-  # For reading from the command line
-    loop = asyncio.get_running_loop()
-    print("Enter messages (type 'exit' to quit):")
-    while True:
-        line = await loop.run_in_executor(None,input,"> ")
-        if line.lower() == "exit":
-            break
-
-
-
-# For reading from a file
-   #  for line in read_lines("CommunicationsModule/TestTXGroundStation"):
-        match line:
-            case "connected uplink mode":
-                await sl.session.handshake(Audimus_pb2.SESSION_MODE.ConnectedUplink)
-            case "connected downlink mode":
-                 await sl.session.handshake(Audimus_pb2.SESSION_MODE.ConnectedDownlink)
-            case "connectionless downlink mode":
-                await sl.session_queue.put(Audimus_pb2.SESSION_MODE.ConnectionlessDownlink)
-            case _ :
-                await AL_tx.put(line)
-        #await asyncio.sleep(1)
-
-
-
 
 async def handle_client(reader=1, writer=2):
-    # application layer queues
-    AL_rx = asyncio.Queue()
-    AL_tx = asyncio.Queue()
 
-    #presentation layer queues
+    # presentation layer queues
     PL_rx = asyncio.Queue()
     PL_tx = asyncio.Queue()
 
-    #session layer queues + state change queue
+    # session layer queues + state change queue
     SL_rx = asyncio.Queue()
     SL_tx = asyncio.Queue()
     SL_SC = asyncio.Queue()
 
-    #data link layer queues
+    # data link layer queues
     DLL_rx = asyncio.Queue()
     DLL_tx = asyncio.Queue()
 
-    #SDR_queue
+    # SDR_queue
     SDR_rx = asyncio.Queue()
     SDR_tx = asyncio.Queue()
 
-    #create instances of each layer, pass each layer its own queue and the queue of the level beneath it
-    al = GroundStationApplicationLayer(AL_rx, AL_tx, PL_rx, PL_tx)
-    pl = GroundStationPresentationLayer(PL_rx, PL_tx, SL_rx, SL_tx)
-    sl = GroundStationSessionLayer(SL_rx, SL_tx, DLL_rx, DLL_tx, SL_SC)
+    # create instances of each layer, pass each layer its own queue and the queue of the level beneath it
     dll = GroundStationDataLinkLayer(DLL_rx, DLL_tx, SDR_rx, SDR_tx, reader, writer)
+    sl = GroundStationSessionLayer(SL_rx, SL_tx, DLL_rx, DLL_tx, SL_SC)
+    pl = GroundStationPresentationLayer(PL_rx, PL_tx, SL_rx, SL_tx)
+    al = GroundStationApplicationLayer(PL_rx, PL_tx, sl)
 
-    #run application layer coroutines
-    al_tx_handler = asyncio.create_task(al.tx())
+    # run application layer coroutines
+    al_tx_handler = asyncio.create_task(al.tx_command_line())
     al_rx_handler = asyncio.create_task(al.rx())
 
-    #run presentation layer coroutines
+    # run presentation layer coroutines
     pl_tx_handler = asyncio.create_task(pl.tx())
     pl_rx_handler = asyncio.create_task(pl.rx())
 
     # session layer coroutines started internally
     sl_handler = asyncio.create_task(sl.start())
 
-    #run data link layer coroutines
-    dll_tx_handler = asyncio.create_task(dll.tx_tcp())
-    dll_rx_handler = asyncio.create_task(dll.rx_tcp())
+    # run data link layer coroutines
+    dll_tx_handler = asyncio.create_task(dll.tx_zmq())
+    dll_rx_handler = asyncio.create_task(dll.rx_zmq())
 
-    # run application rx and tx coroutines
-    AL_rx = asyncio.create_task(app_rx(AL_rx))
-    AL_tx = asyncio.create_task(app_tx(AL_tx, sl))
-
-    # wait for responses
     task_handlers = [
         al_tx_handler,
         al_rx_handler,
